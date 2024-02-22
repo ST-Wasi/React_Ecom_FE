a2c09d3fd9676ece59e62bf02f3aae167a10c189
Wasi <stwasihere@gmail.com>
2024-02-22 16:11:47 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import UserContextProvider from './Context/userContext.jsx'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-    <UserContextProvider>
       <App />
-    </UserContextProvider>
     </BrowserRouter>
 )
